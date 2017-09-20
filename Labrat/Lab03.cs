1723f9d1c4955a5aa9223cac79822400b9d7f783
Antti Kuokkanen <K8183@student.jamk.fi>
2017-09-20 11:00:22 +0300

    Lab03 created

---
+++ Labrat/Lab03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class Lab03
+    {
+        public class Tehtava1
+        {
+            public static bool OnkoLuku(string syote)
+            {
+                if syote 
+                return true;
+            }
+            public static bool OnkoPvm(string syote)
+            {
+
+            }
+
+            public class TestiPeti
+        }
+    }
+}
