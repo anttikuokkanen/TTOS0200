bf7e6592ee79be680b73bee483739363c936f65f
Antti Kuokkanen <K8183@student.jamk.fi>
2017-11-01 10:56:55 +0200

    Demolaskin + tests added

---
+++ DemoLaskin/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.Laskin
+{
+    interface ICalculator
+    {
+        int Add(int number1, int number2);
+        int Multiply(int number1, int number2);
+    }
+    public class MyCalculator : ICalculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public int Divide(int number1, int number2)
+        {
+            return (number1 / number2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testataan omalaskin
+            MyCalculator laskin = new MyCalculator();
+            int luku1 = 10;
+            int luku2 = 20;
+            Console.WriteLine("Lukujen {0} ja {1} summa on {2}", luku1, luku2, laskin.Add(luku1, luku2));
+        }
+    }
+}
---
+++ DemoLaskinTests/MyCalculatorTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JAMK.IT.Laskin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.Laskin.Tests
+{
+    [TestClass()]
+    public class MyCalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            //testataan MyCalculator-luokan Add-metodia, AAA-malli
+            //Arrange
+            int i = 10;
+            int j = 12;
+            int tulos = 22;
+            MyCalculator laskin = new MyCalculator();
+            //Act
+            int summa = laskin.Add(i, j);
+            //Assert
+            Assert.AreEqual(tulos, summa);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            //testataan MyCalculator-luokan Multiply-metodia, AAA-malli
+            //Arrange
+            int i = 10;
+            int j = 12;
+            int tulos = 120;
+            MyCalculator laskin = new MyCalculator();
+            //Act
+            int summa = laskin.Multiply(i, j);
+            //Assert
+            Assert.AreEqual(tulos, summa);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            //tehdään kahden luvun jakamisen testaus
+            int i = 24;
+            int j = 4;
+            int odotettu = 6;
+            MyCalculator laskin = new MyCalculator();
+            //Act
+            int jako = laskin.Divide(i, j);
+            //Assert
+            Assert.AreEqual(odotettu, jako);
+        }
+
+        [TestMethod()]
+        public void DivideTestWithZero()
+        {
+            //tehdään kahden luvun jakamisen testaus
+            int i = 24;
+            int j = 0;
+            int odotettu = 1;
+            MyCalculator laskin = new MyCalculator();
+            //Act
+            try
+            {
+                int jako = laskin.Divide(i, j);
+                Assert.Fail();
+            }
+            //Assert
+            catch (DivideByZeroException)
+            {
+                //ok, tulee oikean tyyppinen poikkeus
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
